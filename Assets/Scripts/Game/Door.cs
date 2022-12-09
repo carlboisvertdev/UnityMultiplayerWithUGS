1cae425cfa475a272cebe76c8757c3c8708924fe
Snappydue <carlboisvert2012@gmail.com>
2022-12-09 16:22:49 -0500

    NetworkVariables

---
+++ Assets/Scripts/Game/Door.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Netcode.Components;
+using UnityEngine;
+
+namespace Game
+{
+    public class Door : NetworkBehaviour
+    {
+        [SerializeField] private List<Switch> _switches;
+        [SerializeField] private NetworkAnimator _animCtrl;
+
+        private Dictionary<Switch, bool> _activeSwitches = new Dictionary<Switch, bool>();
+        
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            if (IsServer)
+            {
+                foreach (Switch doorSwitch in _switches)
+                {
+                    doorSwitch.OnSwitchChanged += OnSwitchChanged;
+                    _activeSwitches.Add(doorSwitch, false);
+                }
+            }
+        }
+
+        private void OnSwitchChanged(Switch doorswitch, bool isactive)
+        {
+            _activeSwitches[doorswitch] = isactive;
+
+            foreach (var doorSwitch in _switches)
+            {
+                if (!_activeSwitches[doorSwitch])
+                {
+                    return;
+                }
+            }
+            
+            Debug.Log("Open the door");
+            _animCtrl.SetTrigger("OpenDoor");
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Switch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Game
+{
+    public class Switch : NetworkBehaviour
+    {
+        private NetworkVariable<bool> _isActive = new NetworkVariable<bool>();
+
+        public delegate void SwitchChanged(Switch doorSwitch, bool isActive);
+        public event SwitchChanged OnSwitchChanged;
+        
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            _isActive.OnValueChanged += OnValueChanged;
+        }
+
+        private void OnValueChanged(bool wasActive, bool isActive)
+        {
+            if (isActive)
+            {
+                Debug.Log("IsActive");
+            }
+            else
+            {
+                Debug.Log("IsNotActive");
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            OnSwitchChangedServerRpc(true);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            OnSwitchChangedServerRpc(false);
+        }
+
+        [ServerRpc]
+        private void OnSwitchChangedServerRpc(bool isActive)
+        {
+            _isActive.Value = isActive;
+            OnSwitchChanged?.Invoke(this, isActive);
+        }
+    }
+}
